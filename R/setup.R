87b4b6e65dcd73652375e12505a88940d5041b2f
Kevin Schaul <kevin.schaul@gmail.com>
2021-02-22 17:07:49 -0500

    Add template files

---
+++ R/setup.R
@@ -0,0 +1,8 @@
+proj_setup <- function(path, ...) {
+  # ensure path exists
+  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+  
+  dir.create(paste0(path, '/data/'), recursive = TRUE, showWarnings = FALSE)
+  dir.create(paste0(path, '/scripts/'), recursive = TRUE, showWarnings = FALSE)
+  dir.create(paste0(path, '/findings/'), recursive = TRUE, showWarnings = FALSE)
+}
