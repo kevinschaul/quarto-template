e65b84ebd9324436a3c168be7a336829bca967a8
Kevin Schaul <kevin.schaul@gmail.com>
2024-04-19 14:26:23 -0500

    update to quarto

--- R/setup.R
+++
@@ -1,7 +0,0 @@
-proj_setup <- function(path, ...) {
-  # ensure path exists
-  dir.create(path, recursive = TRUE, showWarnings = FALSE)
-  
-  dir.create(paste0(path, '/data/'), recursive = TRUE, showWarnings = FALSE)
-  dir.create(paste0(path, '/scripts/'), recursive = TRUE, showWarnings = FALSE)
-}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# rtemplate
-An R markdown template for data journalism projects
+# quarto-template
+
+A Quarto/R template for data journalism projects
+
+## Usage
+
+```
+quarto use template kevinschaul/quarto-template
+```
+
